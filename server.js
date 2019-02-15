bc0410ee33a8fcd19bf839daa89d48e61d480ba0
MaxxKowalik <maxx.kowalik@yahoo.com>
2019-02-15 16:11:46 -0500

    Work on comment_routes
    - Prepare to merge development branch
    - Development branch will contain Ege's work on blog_routes
    - Need blog_routes to test comment_routes
    - Require comment_routes in server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cors = require('cors')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const blogRoutes = require('./app/routes/blog_routes')
+// this will require comment_routes
+const commentRoutes = require('./app/routes/comment_routes')
 
 // require error handling middleware
 const errorHandler = require('./lib/error_handler')
@@ -77,6 +79,7 @@ app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(blogRoutes)
 
+app.use(commentRoutes)
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
 // passed any error messages from them
