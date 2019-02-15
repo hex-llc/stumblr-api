5c39086cd58783d7eebe23e6891743190299175e
discocisco <f.andresort@gmail.com>
2019-02-15 11:34:45 -0500

    Update scripts/comments/update.sh
    - Remove `commenter` key requirement (TOKEN will suffice)

--- scripts/comments/update.sh
+++ scripts/comments/update.sh
@@ -7,12 +7,11 @@ curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
---header "Authorization: Bearer ${TOKEN}" \
---data '{
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
     "comment": {
       "text": "'"${TEXT}"'",
-      "blog": "'"${BLOG}"'",
-      "commenter": "'"${COMMENTER}"'"
+      "blog_id": "'"${BLOG_ID}"'"
     }
   }'
 
